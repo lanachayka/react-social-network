53dfe3586b336c63a4f973e7fde81be1455e35f7
Руслана Підсадюк <ruslana.chaikivska@gail.com>
2021-09-14 19:47:40 +0300

    Added store

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,102 +1,104 @@
-const state = {
-  profile: {
-    postsData: [
-      { id: 1, message: "Hi? how are you?", likeCount: 0 },
-      { id: 2, message: "It's my first post!", likeCount: 23 },
-    ],
-    newPostText: ""
+ const store = {
+  _state: {
+    profile: {
+      postsData: [
+        { id: 1, message: "Hi? how are you?", likeCount: 0 },
+        { id: 2, message: "It's my first post!", likeCount: 23 },
+      ],
+      newPostText: "",
+    },
+    messages: {
+      messagesData: [
+        { id: 1, message: "Hi" },
+        { id: 2, message: "How are you?" },
+        { id: 3, message: "Yo" },
+      ],
+      dialogsData: [
+        {
+          id: 1,
+          userName: "Dimych",
+          photo:
+            "https://st2.depositphotos.com/3873339/8013/i/600/depositphotos_80131050-stock-photo-realistic-square-picture-frame-on.jpg",
+        },
+        {
+          id: 2,
+          userName: "Max",
+          photo:
+            "https://st2.depositphotos.com/3873339/8013/i/600/depositphotos_80131050-stock-photo-realistic-square-picture-frame-on.jpg",
+        },
+        {
+          id: 3,
+          userName: "Anna",
+          photo:
+            "https://st2.depositphotos.com/3873339/8013/i/600/depositphotos_80131050-stock-photo-realistic-square-picture-frame-on.jpg",
+        },
+        {
+          id: 4,
+          userName: "Sveta",
+          photo:
+            "https://st2.depositphotos.com/3873339/8013/i/600/depositphotos_80131050-stock-photo-realistic-square-picture-frame-on.jpg",
+        },
+        {
+          id: 5,
+          userName: "Sasha",
+          photo:
+            "https://st2.depositphotos.com/3873339/8013/i/600/depositphotos_80131050-stock-photo-realistic-square-picture-frame-on.jpg",
+        },
+        {
+          id: 6,
+          userName: "Victor",
+          photo:
+            "https://st2.depositphotos.com/3873339/8013/i/600/depositphotos_80131050-stock-photo-realistic-square-picture-frame-on.jpg",
+        },
+      ],
+    },
+    navbar: {
+      friends: [
+        {
+          id: 1,
+          name: "Dimych",
+          photo:
+            "https://st2.depositphotos.com/3873339/8013/i/600/depositphotos_80131050-stock-photo-realistic-square-picture-frame-on.jpg",
+        },
+        {
+          id: 2,
+          name: "Max",
+          photo:
+            "https://st2.depositphotos.com/3873339/8013/i/600/depositphotos_80131050-stock-photo-realistic-square-picture-frame-on.jpg",
+        },
+        {
+          id: 3,
+          name: "Sveta",
+          photo:
+            "https://st2.depositphotos.com/3873339/8013/i/600/depositphotos_80131050-stock-photo-realistic-square-picture-frame-on.jpg",
+        },
+      ],
+    },
   },
-  messages: {
-    messagesData: [
-      { id: 1, message: "Hi" },
-      { id: 2, message: "How are you?" },
-      { id: 3, message: "Yo" },
-    ],
-    dialogsData: [
-      {
-        id: 1,
-        userName: "Dimych",
-        photo:
-          "https://st2.depositphotos.com/3873339/8013/i/600/depositphotos_80131050-stock-photo-realistic-square-picture-frame-on.jpg",
-      },
-      {
-        id: 2,
-        userName: "Max",
-        photo:
-          "https://st2.depositphotos.com/3873339/8013/i/600/depositphotos_80131050-stock-photo-realistic-square-picture-frame-on.jpg",
-      },
-      {
-        id: 3,
-        userName: "Anna",
-        photo:
-          "https://st2.depositphotos.com/3873339/8013/i/600/depositphotos_80131050-stock-photo-realistic-square-picture-frame-on.jpg",
-      },
-      {
-        id: 4,
-        userName: "Sveta",
-        photo:
-          "https://st2.depositphotos.com/3873339/8013/i/600/depositphotos_80131050-stock-photo-realistic-square-picture-frame-on.jpg",
-      },
-      {
-        id: 5,
-        userName: "Sasha",
-        photo:
-          "https://st2.depositphotos.com/3873339/8013/i/600/depositphotos_80131050-stock-photo-realistic-square-picture-frame-on.jpg",
-      },
-      {
-        id: 6,
-        userName: "Victor",
-        photo:
-          "https://st2.depositphotos.com/3873339/8013/i/600/depositphotos_80131050-stock-photo-realistic-square-picture-frame-on.jpg",
-      },
-    ],
+  getState() {
+    return this._state;
   },
-  navbar: {
-    friends: [
-      {
-        id: 1,
-        name: "Dimych",
-        photo:
-          "https://st2.depositphotos.com/3873339/8013/i/600/depositphotos_80131050-stock-photo-realistic-square-picture-frame-on.jpg",
-      },
-      {
-        id: 2,
-        name: "Max",
-        photo:
-          "https://st2.depositphotos.com/3873339/8013/i/600/depositphotos_80131050-stock-photo-realistic-square-picture-frame-on.jpg",
-      },
-      {
-        id: 3,
-        name: "Sveta",
-        photo:
-          "https://st2.depositphotos.com/3873339/8013/i/600/depositphotos_80131050-stock-photo-realistic-square-picture-frame-on.jpg",
-      },
-    ],
+  _callSubscriber() {
+    console.log("State changed");
+  },
+   addPost() {
+    const newPost = {
+      id: this._state.profile.postsData[this._state.profile.postsData.length - 1] +
+      1,
+      message: this._state.profile.newPostText,
+      likeCount: 0,
+    };
+    this._state.profile.postsData.push(newPost);
+    this._state.profile.newPostText = "";
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profile.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
   },
 };
 
-let rerenderEntireTree = (state) => {
-  console.log("State changed");
-};
-
-export const addPost = () => {
-  const newPost = {
-    id: state.profile.postsData[state.profile.postsData.length - 1] + 1,
-    message: state.profile.newPostText,
-    likeCount: 0,
-  };
-  state.profile.postsData.push(newPost);
-  state.profile.newPostText = "";
-  rerenderEntireTree(state);
-};
-
-export const updateNewPostText = (newText) => {
-  state.profile.newPostText = newText;
-  rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
- rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;
